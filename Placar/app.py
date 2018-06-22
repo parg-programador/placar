e45818d0fad7c644313a39b39167015b7df2e87b
Pablo XBrindes <xadmin@xbrindes.com.br>
2018-06-22 16:42:36 -0300

    votação funcionando

--- Placar/app.py
+++ Placar/app.py
@@ -9,10 +9,20 @@ import sys
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
     # Debug mode will enable more verbose output in the console window.
     # It must be set at the beginning of the script.
     bottle.debug(True)
+
+@bottle.route('/static/<filepath:path>')
+def server_static(filepath):
+    """Handler for static files, used with the development server.
+    When running under a production server such as IIS or Apache,
+    the server should be configured to serve the static files."""
+    return bottle.static_file(filepath, root=STATIC_ROOT)
 
 def wsgi_app():
     """Returns the application to make available through wfastcgi. This is used
@@ -20,20 +30,11 @@ def wsgi_app():
     return bottle.default_app()
 
 if __name__ == '__main__':
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-
-    @bottle.route('/static/<filepath:path>')
-    def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
 
     # Starts a local test server.
     bottle.run(server='wsgiref', host=HOST, port=PORT)
